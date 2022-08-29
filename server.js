336a43345c9b1a6b71ded9edf4d29ff230e9f87e
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-29 22:30:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const http = require('http');
+const { authors, books } = require('./fixtures');
 
-const HOSTNAME = 'localhost';
+const reqHand = (req, res) => {
+	if (req.url == '/books') {
+		res.writeHead(200);
+		res.end(JSON.stringify(books));
+	} else if (req.url == '/authour') {
+		res.writeHead(200);
+		res.end(JSON.stringify(authors));
+	} else {
+		res.writeHead(404);
+		res.end('Invalid Route ');
+	}
+};
+
+const server = http.createServer(reqHand);
 const PORT = 8000;
-
-function requestHander(req, res) {
-	console.log(req);
-	res.end('Hello from the Server');
-}
-
-const server = http.createServer(requestHander);
-
-server.listen(PORT, HOSTNAME, () => {
-	console.log(`Server started ! at  http://${HOSTNAME}:${PORT}`);
+server.listen(PORT, () => {
+	console.log(`Server started ! at Port ${PORT}`);
 });
