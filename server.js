50086534f8f926111b5f436f0dc4e1762286075a
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-29 21:15:05 +0100

    Installation of Nodemon

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const http = require('http');
 const HOSTNAME = 'localhost';
 const PORT = 8000;
 
+const requestHander = (req, res) => {
+	console.log(req);
+};
+
 const server = http.createServer();
 
 server.listen(PORT, HOSTNAME, () => {
