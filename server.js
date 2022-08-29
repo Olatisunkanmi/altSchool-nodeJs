761c4e2a0f663e90239fb618b8455d4763f709e9
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-29 21:27:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const http = require('http');
 const HOSTNAME = 'localhost';
 const PORT = 8000;
 
-const requestHander = (req, res) => {
+function requestHander(req, res) {
 	console.log(req);
-};
+	res.end('Hello from the Server');
+}
 
-const server = http.createServer();
+const server = http.createServer(requestHander);
 
 server.listen(PORT, HOSTNAME, () => {
 	console.log(`Server started ! at  http://${HOSTNAME}:${PORT}`);
