5670916256a019da8dd222750653e2823180dd2c
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-29 22:41:26 +0100

    Week 1 Over!

---
+++ fixtures.js
@@ -0,0 +1,46 @@
+//Let's create some JSON Data
+const books = [
+    {
+        title: "The Design of Everyday Things",
+        author: "Don Norman",
+        year: 2014,
+        id: 1
+    },
+    {
+        title: "Eloquent JavaScript, 3rd Edition: A Modern Introduction to Programming",
+        author: "Marijn Haverbeke",
+        year: 2014,
+        id: 2
+    },
+    {
+        title: "Algorithms to Live By",
+        author: "Brian Christian and Thomas L. Griffiths",
+        year: 2016,
+        id: 3
+    }
+];
+
+const authors = [
+    {
+        name: "Don Norman",
+        country: "USA",
+        id: 1
+    },
+    {
+        name: "Marijn Haverbeke",
+        country: "Netherlands",
+        id: 2
+    },
+    {
+        name: "Brian Christian and Thomas L. Griffiths",
+        country: "USA",
+        id: 3
+    }
+];
+
+module.exports = {
+    books,
+    authors
+};
+
+        
