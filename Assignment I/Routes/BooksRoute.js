fb3b4c37db1d88d9025791bdbac77b5f5b9ac6c2
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-02 16:14:50 +0100

    Update BooksRoute.js

--- Assignment I/Routes/BooksRoute.js
+++ Assignment I/Routes/BooksRoute.js
@@ -1,5 +1,11 @@
 const booksControllers = require('../Controllers/BooksController');
 const router = require('express').Router();
 
-router.route('/').get(booksControllers.getAllbooks);
+router
+	.route('/')
+	.get(booksControllers.getAllbooks)
+	.post(booksControllers.checkId, booksControllers.createBook);
+
+// router.route('/:id');
+
 module.exports = router;
