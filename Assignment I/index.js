dbaca025f2939716583f694c2ec4912bb8388be0
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-16 11:19:18 +0100

    Update index.js

--- Assignment I/index.js
+++ Assignment I/index.js
@@ -15,4 +15,11 @@ app.use(morgan('common'));
 app.use('/api/books', BOOKSROUTES);
 app.use('/api/users', USERROUTES);
 app.use('/api/admin', ADMINROUTES);
+
+app.use('*', (req, res, next) => {
+	res.status(404).json({
+		status: 'Error',
+	});
+});
+
 module.exports = app;
