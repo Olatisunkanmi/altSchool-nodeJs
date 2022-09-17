b96da5f3a83e048487816e70fbd3ddd6dfdabaed
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-17 19:03:40 +0100

    Update index.js

--- Assignment G/index.js
+++ Assignment G/index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const bookControllers = require('./controllers/BookController');
 const fs = require('fs');
 const path = require('path');
+const { stringify } = require('querystring');
 const booksDbPath = path.join(__dirname, 'db', 'books.json');
 
 const reqHand = (req, res) => {
@@ -14,10 +15,31 @@ const reqHand = (req, res) => {
 		console.log(' UPDATE ');
 	} else if (req.url === '/echo' && req.method === 'POST') {
 		sendEcho(req, res);
+	} else {
+		res.statusCode = 404;
+		res.end();
 	}
 };
 
-const sendEcho = (req, res) => {};
+const sendEcho = (req, res) => {
+	let body = [];
+	req
+		.on('err', (err) => {
+			console.error(err);
+		})
+		.pipe(res);
+
+	res.on('err', (err) => {
+		console.error(err);
+	});
+
+	res.writeHead(200, {
+		'Content-Type': 'applicatiion/json',
+		'X-powered-By': 'bacon',
+		session: 'Echo Session',
+		Connection: 'Keep-Alive',
+	});
+};
 
 module.exports = {
 	http,
