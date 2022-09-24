4384c59abaa51a53964b408631bcb389a9873b6e
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-24 23:47:56 +0100

    Update index.js

--- Assignment J  - Mongo DB/index.js
+++ Assignment J  - Mongo DB/index.js
@@ -19,6 +19,29 @@ app.use('*', (req, res, next) => {
 	});
 });
 
+app.all('*', (req, res, next) => {
+	// res.status(404).json({
+	// 	status: 'Fail',
+	// });
+
+	const err = new Error(`Cant find ${req.originalUrl}`);
+	err.status = 'fail';
+	err.statusCode = 404;
+
+	next(err);
+});
+
+app.use((err, req, res, next) => {
+	// Default error status code and status
+	err.statusCode = err.statusCode || 500;
+
+	err.status = err.status || 'Erro r';
+	res.status(err.statusCode).json({
+		status: err.status,
+		message: err.message,
+	});
+});
+
 module.exports = {
 	app,
 	http,
