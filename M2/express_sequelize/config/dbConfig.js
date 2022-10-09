dcc820750250a6a5b00058f09d0ccbd59aa1eb84
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-10-09 21:33:07 +0100

    Update dbConfig.js

--- M2/express_sequelize/config/dbConfig.js
+++ M2/express_sequelize/config/dbConfig.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const CONFIG = {
 	DB_NAME: process.env.DB_NAME,
 	DB_USER: process.env.DB_USER,
-	DB_PASSOWRD: process.env.DB_PASSOWRD,
+	DB_PASSWORD: process.env.DB_PASSWORD,
 	DB_DIALECT: process.env.DB_DIALECT,
 	DB_HOST: process.env.DB_HOST,
 };
