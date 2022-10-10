2a558cbd9af60604c45d41f4b4ef3fed1945d98d
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-10-10 07:23:40 +0100

    Update books.js

--- M2/express_sequelize/routes/books.js
+++ M2/express_sequelize/routes/books.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
 const bookController = require('../controller/bookController');
 
-router.route('/').get(bookController.getAllBooks);
+router
+	.route('/')
+	.get(bookController.getAllBooks)
+	.post(bookController.createBook);
+router
+	.route('/:id')
+	.put(bookController.updateBook)
+	.delete(bookController.deleteBook);
 
 module.exports = router;
