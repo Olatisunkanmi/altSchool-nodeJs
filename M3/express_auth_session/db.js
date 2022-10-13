51c92ac07017ff201feaefb76a83c1e30c84a5ba
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-10-13 12:20:58 +0100

    Update db.js

--- M3/express_auth_session/db.js
+++ M3/express_auth_session/db.js
@@ -1,19 +1,19 @@
 const moogoose = require('mongoose');
 require('dotenv').config();
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGO_URL = process.env.MONGO_URL;
 
 // connect to mongodb
 function connectToMongoDB() {
-    moogoose.connect(MONGODB_URI);
+	moogoose.connect(MONGO_URL);
 
-    moogoose.connection.on('connected', () => {
-        console.log('Connected to MongoDB successfully');
-    });
+	moogoose.connection.on('connected', () => {
+		console.log('Connected to MongoDB successfully');
+	});
 
-    moogoose.connection.on('error', (err) => {
-        console.log('Error connecting to MongoDB', err);
-    })
+	moogoose.connection.on('error', (err) => {
+		console.log('Error connecting to MongoDB', err);
+	});
 }
 
 module.exports = { connectToMongoDB };
