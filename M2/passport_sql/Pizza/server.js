9e92cd98a54d4e499b1aa7f4072465bfc382b4ea
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-10-19 12:18:02 +0100

    Initial_changes

---
+++ M2/passport_sql/Pizza/server.js
@@ -0,0 +1,20 @@
+const { createServer } = require('http');
+const app = require('./app');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Create server class
+class Server extends createServer {
+	constructor(app, port) {
+		super();
+		this.port = port;
+		this.app = app;
+
+		createServer(this.app).listen(this.port, () => {
+			console.log(`Node is listening on ${this.port}`);
+		});
+	}
+}
+
+// sever listen.
+const server_one = new Server(app, 3000).listen();
