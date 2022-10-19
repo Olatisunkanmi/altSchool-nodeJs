d73d67db7dc45e390813e3af254380c1b80a7471
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-10-19 16:25:33 +0100

    Create orderController.js

---
+++ M3/passport_sql/Pizza/Controllers/orderController.js
@@ -0,0 +1,70 @@
+const db = require('../utils/db');
+
+// declare
+var Orders;
+
+// Get Models
+const OrderModel = db.Orders;
+
+exports.getAllOrders = async (req, res) => {
+	try {
+		Orders = await db.Orders.findAll();
+		res.status(200).json(Orders);
+	} catch (error) {
+		console.log(error);
+		res.status(500).send(error);
+	}
+};
+
+exports.createOrder = async (req, res) => {
+	const { title, year, isbn } = req.body;
+	// const OrderInfo = req.body;
+
+	try {
+		Orders = await OrderModel.build({
+			title: title,
+			year: year,
+			isbn: isbn,
+		});
+		await Orders.save();
+		res.status(200).json({
+			status: 'Success',
+			data: Orders,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(400).send(error);
+	}
+};
+
+exports.updateOrder = async (req, res, next) => {
+	const OrderId = req.params.id * 1;
+	const updateInfo = req.body;
+	try {
+		const Order = await OrderModel.update(updateInfo, {
+			where: {
+				id: OrderId,
+			},
+		});
+		res.status(200).json({
+			status: 'success',
+			data: Order,
+		});
+	} catch (error) {
+		res.status(500).send(error);
+	}
+};
+
+exports.deleteOrder = async (req, res, next) => {
+	const OrderId = req.params.id * 1;
+	try {
+		const Order = await OrderModel.destroy({
+			where: {
+				id: OrderId,
+			},
+		});
+		res.status(200).json('Order deleted');
+	} catch (error) {
+		res.status(500).send(error);
+	}
+};
