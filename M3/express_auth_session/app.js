ba20febf4e438884cf7382f037eece9be4bd374e
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-10-19 10:10:18 +0100

    update

--- M3/express_auth_session/app.js
+++ M3/express_auth_session/app.js
@@ -1,17 +1,19 @@
 const express = require('express');
-const passport = require('passport');  // authentication
+const passport = require('passport'); // authentication
 const connectEnsureLogin = require('connect-ensure-login'); //authorization middleware
 const bodyParser = require('body-parser');
-
+const morgan = require('morgan');
 const userModel = require('./models/users');
+const { promisify } = require('util');
 
-const session = require('express-session');  //session middleware
+const session = require('express-session'); //session middleware
 require('dotenv').config();
 
 const db = require('./db');
 
 const PORT = 3000;
 const app = express();
+app.use(morgan('common'));
 
 // Connect to MongoDB
 db.connectToMongoDB();
@@ -22,12 +24,14 @@ const booksRoute = require('./routes/books');
 // Session is a way to store data on the server between requests
 // so that we can access it on subsequent requests
 // in this case, we are storing the authenticated user id for the duration of the session
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 60 * 60 * 1000 } // 1 hour
-}));
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: true,
+		cookie: { maxAge: 60 * 60 * 1000 }, // 1 hour
+	}),
+);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -47,55 +51,60 @@ app.set('view engine', 'ejs');
 app.use('/books', connectEnsureLogin.ensureLoggedIn(), booksRoute);
 
 // renders the home page
-app.get('/', connectEnsureLogin.ensureLoggedIn(),  (req, res) => {
-    res.render('index');
+app.get('/', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
+	res.render('index');
 });
 
 // renders the login page
 app.get('/login', (req, res) => {
-    res.render('login');
+	res.render('login');
 });
 
 // renders the signup page
 app.get('/signup', (req, res) => {
-    res.render('signup');
+	res.render('signup');
 });
 
 // handles the signup request for new users
 app.post('/signup', (req, res) => {
-    const user = req.body;
-    userModel.register(new userModel({ username: user.username }), user.password, (err, user) => {
-        if (err) {
-            console.log(err);
-            res.status(400).send(err);
-        } else {
-            passport.authenticate('local')(req, res, () => {
-                res.redirect("/books")
-            });
-        }
-    });
+	const user = req.body;
+	userModel.register(
+		new userModel({ username: user.username }),
+		user.password,
+		(err, user) => {
+			if (err) {
+				console.log(err);
+				res.status(400).send(err);
+			} else {
+				passport.authenticate('local')(req, res, () => {
+					res.redirect('/books');
+				});
+			}
+		},
+	);
 });
-
 
 // handles the login request for existing users
-app.post('/login', passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
-    res.redirect('/books');
-});
+app.post(
+	'/login',
+	passport.authenticate('local', { failureRedirect: '/' }),
+	(req, res) => {
+		res.redirect('/books');
+	},
+);
 
 // handles the logout request
 app.post('/logout', (req, res) => {
-    req.logout();
-    res.redirect('/');
+	req.logout();
+	res.redirect('/');
 });
-
 
 //catch errors middleware
 app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(500).send('Something broke!');
+	console.log(err);
+	res.status(500).send('Something broke!');
 });
 
-
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+	console.log(`Server is running on port ${PORT}`);
+});
