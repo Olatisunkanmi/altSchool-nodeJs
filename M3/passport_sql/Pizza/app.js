4cac6b1d82542a888c3a7bd00361f2e5f3b41395
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-10-19 16:25:27 +0100

    Update app.js

--- M3/passport_sql/Pizza/app.js
+++ M3/passport_sql/Pizza/app.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const bookRouter = require('./routes/bookRoutes');
 
 const app = express();
 app.use(express.json());
+
+app.use('/books', bookRouter);
+
+app.get('/', (req, res) => {
+	res.send('Welcome to the book API');
+});
+
+app.use((err, req, res, next) => {
+	console.log(err);
+	res.status(400).json({
+		error: err.message,
+		stack: err.stack,
+	});
+});
 
 module.exports = app;
