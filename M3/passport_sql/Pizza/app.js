fe6996be4b1eb4fea3206613b783fcdae4ffb897
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-10-23 13:48:28 +0100

    ...

--- M3/passport_sql/Pizza/app.js
+++ M3/passport_sql/Pizza/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const bookRouter = require('./routes/bookRoutes');
+const ORDERROUTER = require('./routes/orderRoutes');
+// const USERROUTER = require('./routes/userRoutes');
+const morgan = require('morgan');
 
 const app = express();
 app.use(express.json());
+app.use(morgan('common'));
 
-app.use('/books', bookRouter);
+// app.use('/api/v1/orders', ORDERROUTER);
+// app.use('/api/v1/users', USERROUTER);
 
 app.get('/', (req, res) => {
 	res.send('Welcome to the book API');
