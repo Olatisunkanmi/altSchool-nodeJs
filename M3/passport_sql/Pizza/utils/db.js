fd297c26fe0ea920d571441568b09c1bec674a41
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-10-23 15:14:40 +0100

    Update db.js

--- M3/passport_sql/Pizza/utils/db.js
+++ M3/passport_sql/Pizza/utils/db.js
@@ -1,5 +1,25 @@
 require('dotenv').config();
 const { Sequelize, DataTypes } = require('sequelize');
 const CONFIG = require('./dbConffig');
+const AppErr = require('./AppError');
 
-const sequelize = new Sequelize();
+const sequelize = new Sequelize(
+	CONFIG.DB_NAME,
+	CONFIG.DB_USER,
+	CONFIG.DB_PASSWORD,
+	{
+		host: CONFIG.DB_HOST,
+		dialect: CONFIG.DB_DIALECT,
+	},
+);
+
+const seqConnect = (next) => {
+	sequelize
+		.authenticate()
+		.then(() => {
+			console.log('DB connected ');
+		})
+		.catch((err) => console.log(err));
+};
+
+process.nextTick(seqConnect);
